29e27917a2c401a4ddddfd3d3ad5f4078af8043c
Omar El Halabi <omar_halabi25@hotmail.com>
2020-04-27 23:54:15 +0300

    feat: Show full name in toolbar when ChatDetailFragment is shown
    This commit uses a separate toolbar for flexibility. It also setups the
    toolbar with the navigation controller in MainActivity.
    MainActivity listens to destination changes in the navigation graph and
    sets the toolbar title to chat user's full name in ChatDetailFragment
    through the passed bundle argument to the fragment.
    It also uses fragment tag instead of FragmentContainerView due to a bug
    when it's used with NavHostFragment

--- app/src/main/java/com/github/whatsmock/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/whatsmock/ui/main/MainActivity.kt
@@ -2,14 +2,37 @@ package com.github.whatsmock.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.github.whatsmock.R
+import com.github.whatsmock.data.vo.Chat
 import dagger.android.AndroidInjection
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+
+        val navController = findNavController(R.id.fragment)
+
+        toolbar.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, arguments ->
+            when (destination.id) {
+                R.id.chatsFragment -> {
+                    toolbar.title = getString(R.string.app_name)
+                }
+                R.id.chatDetailFragment -> {
+                    toolbar.title = (arguments?.get("chat") as? Chat)?.fullName ?: "Chat"
+                }
+            }
+        }
     }
 }
