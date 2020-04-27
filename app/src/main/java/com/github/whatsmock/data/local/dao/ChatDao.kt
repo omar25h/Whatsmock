98172ab08facc91bc54477d41f3da93b8e0b75b8
Omar El Halabi <omar_halabi25@hotmail.com>
2020-04-27 15:42:41 +0300

    fix: Get chats ordered by message time descending, then by name ascending

--- app/src/main/java/com/github/whatsmock/data/local/dao/ChatDao.kt
+++ app/src/main/java/com/github/whatsmock/data/local/dao/ChatDao.kt
@@ -7,6 +7,10 @@ import com.github.whatsmock.data.local.view.Chat
 
 @Dao
 interface ChatDao {
-    @Query("SELECT * FROM chat")
+    /**
+     * getChats will retrieve list of chats using the #link{chat} database view.
+     * Results will be ordered by last message sent in descending order, and then by fullName value
+     */
+    @Query("SELECT * FROM chat ORDER BY updatedAt DESC, fullName ASC")
     fun getChats(): LiveData<List<Chat>>
 }
